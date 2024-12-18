3c33c09af520cf870c6e3c6b23d805d0845eaeb5
sapr797 <unisapr797@gmail.com>
2024-12-18 11:43:19 +0300

    добавлен файл main.cpp
    добавлен файл main.cpp сейчас

---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    double radius;
+    std::cout << "Введите радиус круга: ";
+    std::cin >> radius;
+
+    if (std::cin.fail() || radius < 0) {
+        std::cerr << "Неверный ввод. Радиус должен быть положительным числом." << std::endl;
+        return 1;
+    }
+
+    double area = M_PI * radius * radius;
+    std::cout << "Площадь круга с радиусом " << radius << " равна " << area << std::endl;
+
+    return 0;
+}
